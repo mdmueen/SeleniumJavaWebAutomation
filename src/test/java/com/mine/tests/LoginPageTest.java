2b217ba0f179de7e90f9876e00b1bd907ced77a5
mdmueen <mdmueen@gmail.com>
2024-03-10 21:54:39 -0400

    Updated LoginPageTest

--- src/test/java/com/mine/tests/LoginPageTest.java
+++ src/test/java/com/mine/tests/LoginPageTest.java
@@ -2,12 +2,13 @@ package com.mine.tests; // This package contains all test cases
 
 import com.mine.driver.DriverManager;
 import com.mine.utils.ReadPropertyFile;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
 
 public final class LoginPageTest extends BaseTest {
 
@@ -16,28 +17,47 @@ public final class LoginPageTest extends BaseTest {
     @Test()
     public void test1() throws Exception {
 
-        WebDriverWait explicitWait = new WebDriverWait(DriverManager.getDriver(), 10);
+        WebDriverWait explicitWait = new WebDriverWait(DriverManager.getDriver(), 10); //WebDriverWait is a class which extends FluetWait class
         By headerMenu_InputForms = By.xpath("//a[@class='dropdown-toggle' and contains(text(),'Input Forms')]");
         By subMenu_InputForms_SimpleFormDemo = By.xpath("//a[@class='dropdown-toggle' and contains(text(),'Input Forms')]/following-sibling::ul/li[1]/a");
         // By subMenu_InputForms_CheckboxDemo = By.xpath("//a[@class='dropdown-toggle' and contains(text(),'Input Forms')]/following-sibling::ul/li[2]/a");
 
-        // below is another xpath for above element using normalize-space() method of xpath
+        // below is another xpath for above elements using normalize-space() method of xpath
         By subMenu_InputForms_CheckboxDemo = By.xpath("//ul[@class='dropdown-menu']//a[normalize-space()='Checkbox Demo']");
         By subMenu_InputForms_RadioButtonsDemo = By.xpath("//ul[@class='dropdown-menu']//a[normalize-space()='Radio Buttons Demo']");
 
         DriverManager.getDriver().get(ReadPropertyFile.getValue("Url"));
-        System.out.println(DriverManager.getDriver().getTitle()); // have some assertion whether landing page is opened as expected or not
+        String title_LandingPage = DriverManager.getDriver().getTitle(); // have some assertion whether landing page is opened as expected or not
+        Assertions.assertThat(title_LandingPage)
+                .isNotNull()
+                .containsIgnoringCase("best demo website"); // Assertions is a class in AssertJ library with overloaded assertThat() methods
 
         explicitWait.until(ExpectedConditions.visibilityOfElementLocated(headerMenu_InputForms)).click();
         DriverManager.getDriver().findElement(subMenu_InputForms_SimpleFormDemo).click();
-        System.out.println(DriverManager.getDriver().getTitle());
+        String title_SimpleFormPage = DriverManager.getDriver().getTitle();
+
+        Assertions.assertThat(title_SimpleFormPage)
+                .containsIgnoringCase("Simple Form");
+        Assert.assertTrue(title_SimpleFormPage.toLowerCase().contains("simple form")); // TestNG assertion for the above AssertJ assertion. For TestNG assertion we need to give all in lower case or upper case
 
         DriverManager.getDriver().findElement(headerMenu_InputForms).click();
         DriverManager.getDriver().findElement(subMenu_InputForms_CheckboxDemo).click();
-        System.out.println(DriverManager.getDriver().getTitle());
+        String title_CheckBoxDemoPage = DriverManager.getDriver().getTitle();
+
+        /*SoftAssertions softlyAssert = new SoftAssertions(); // soft assertion using AssertJ lib
+        softlyAssert.assertThat(title_CheckBoxDemoPage)
+                .isNotNull();
+        softlyAssert.assertAll();*/
+
+        /*SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(title_CheckBoxDemoPage).isNotNull();
+        });*/
 
         DriverManager.getDriver().findElement(headerMenu_InputForms).click();
         DriverManager.getDriver().findElement(subMenu_InputForms_RadioButtonsDemo).click();
-        System.out.println(DriverManager.getDriver().getTitle());
+        String title_RadioButtonPage = DriverManager.getDriver().getTitle();
+
+        Assertions.assertThat(title_RadioButtonPage)
+                .as("Title does not start with the words as expected").startsWithIgnoringCase("seleniumEasy");
     }
 }
