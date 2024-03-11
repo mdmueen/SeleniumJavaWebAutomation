4f08affc0eec7b51d093dc4825fbf4392207edf0
mdmueen <mdmueen@gmail.com>
2024-03-11 01:16:25 -0400

    Updated SimpleFormDemoPage file

--- src/main/java/com/seleniumEasy/pages/SimpleFormDemoPage.java
+++ src/main/java/com/seleniumEasy/pages/SimpleFormDemoPage.java
@@ -2,6 +2,11 @@ package com.seleniumEasy.pages;
 
 import com.mine.driver.DriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public final class SimpleFormDemoPage {
 
@@ -15,11 +20,14 @@ public final class SimpleFormDemoPage {
     }
 
     public SimpleFormDemoPage enterValue1(int value){
+
+        DriverManager.getDriver().findElement(textboxEnterValue1).clear();
         DriverManager.getDriver().findElement(textboxEnterValue1).sendKeys(String.valueOf(value));
         return this; // achieving method chaining by returning anonymous class object without reference by using - new SimpleFormDemoPage() or by using 'this' keyword
     }
 
     public SimpleFormDemoPage enterValue2(int value){
+        DriverManager.getDriver().findElement(textboxEnterValue2).clear();
         DriverManager.getDriver().findElement(textboxEnterValue2).sendKeys(String.valueOf(value));
         return this;
     }
@@ -29,6 +37,9 @@ public final class SimpleFormDemoPage {
     }
 
     public String getDisplayTotal(){
+        WebDriverWait wait = new WebDriverWait(DriverManager.getDriver(), 10);
+        wait.until(d -> d.findElement(labelTotal).isEnabled()); // using Java 8 Functional programming way
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(labelTotal));
         return DriverManager.getDriver().findElement(labelTotal).getText();
     }
 
